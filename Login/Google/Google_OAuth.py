76c1a28b4c53d0fe142ecf207529f43b65e74d24
SuhaiBatman <97989770+SuhaiBatman@users.noreply.github.com>
2023-10-03 15:03:33 -0500

    added backend to laptop

--- Login/Google/Google_OAuth.py
+++ Login/Google/Google_OAuth.py
@@ -6,6 +6,7 @@ from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
+import concurrent.futures
 
 app = Flask("Google Login App")
 app.secret_key = "CodeSpecialist.com"
@@ -47,28 +48,36 @@ def validate(auth_token): ##revalid token if time is off
     except:
         return "The token is either invalid or has expired"
 
-@app.route("/callback") ##validates token and redirects to Google
+@app.route("/callback")
 def callback():
-    validate(flow.fetch_token(authorization_response=request.url))
+    auth_token = flow.fetch_token(authorization_response=request.url)
 
     if not session["state"] == request.args["state"]:
         abort(500)  # State does not match
 
-    credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    def validate_and_get_id_info():
+        id_info = validate(auth_token)
+        credentials = flow.credentials
+        request_session = requests.session()
+        cached_session = cachecontrol.CacheControl(request_session)
+        token_request = google.auth.transport.requests.Request(session=cached_session)
 
-    id_info = id_token.verify_oauth2_token( ##receive user info and store
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
-    )
+        id_info = id_token.verify_oauth2_token(
+            id_token=credentials._id_token,
+            request=token_request,
+            audience=GOOGLE_CLIENT_ID
+        )
 
-    session["google_id"] = id_info.get("sub") ##use sessions to display and store user info
+        return id_info
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        id_info = executor.submit(validate_and_get_id_info).result()
+
+    session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
     session["email"] = id_info.get("email")
     return redirect("/protected_area")
+
 
 @app.route("/logout") ##logout
 def logout():
