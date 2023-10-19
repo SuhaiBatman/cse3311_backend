0fa39ed88bcec44d1e85954b806ba893b71c6fe6
SuhaiBatman <97989770+SuhaiBatman@users.noreply.github.com>
2023-10-18 23:10:54 -0500

    need to implement TTL for unverified emails in DB

--- Login/Manual/Login.py
+++ Login/Manual/Login.py
@@ -26,7 +26,7 @@ totp_secret = os.getenv("TOTP_SECRET")
 def home():
     return 'Welcome to the Flask Login System'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form['email']
@@ -40,17 +40,16 @@ def signup():
         user = users_collection.find_one({'email': email})
         
         if user:
-            if not password:
-                flash('Password cannot be empty. Please provide a password.')
-                return redirect(url_for('signup'))
-            
             # Hash the password using bcrypt
             hashed_password = bcrypt.generate_password_hash(password).decode(os.getenv("DECODE_ALGORITHM"))
-            
-            # Update the user's password
-            users_collection.update_one({'email': email}, {'$set': {'password': hashed_password}})
-            flash('Password updated successfully.')
-            return redirect(url_for('login'))
+            if user["password"] == "":
+                # Update the user's password
+                users_collection.update_one({'email': email}, {'$set': {'password': hashed_password}})
+                flash('Password updated successfully.')
+                return redirect(url_for('login'))
+            elif bcrypt.check_password_hash(user['password'], password):
+                flash("user already has an account. Please log in")
+                return redirect(url_for('login'))
         else:
             if not password:
                 flash('Password is required.')
@@ -74,32 +73,41 @@ def login():
         password = request.form['password']
         user = users_collection.find_one({'email': email})
         
-        if user and bcrypt.check_password_hash(user['password'], password):
-            # The password is verified using check_password_hash
-            # Generate a TOTP token
-            totp = pyotp.TOTP(totp_secret)
-            token = totp.now()
+        if user:
+            # Check if the password field is not empty
+            if not password:
+                flash('Password cannot be empty. Please provide a password.')
+                return redirect(url_for('login'))
             
-            # Send the TOTP token to the user's email
-            message = Mail(
-                from_email='dev.pixera@gmail.com',
-                to_emails=[email],
-                subject='Two-Factor Authentication',
-                plain_text_content=f'Your TOTP token is: {token}'
-            )
-            
-            try:
-                sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
-                response = sg.send(message)
-                flash('A TOTP token has been sent to your email. Please check your email and enter the token.')
-            except Exception:
-                flash('Failed to send the TOTP token.')
-            
-            # Redirect to the 2FA verification page
-            return redirect(url_for('verify_2fa', email=email))
-        
+            # Verify the password using check_password_hash
+            if user["password"] != "":
+                if bcrypt.check_password_hash(user['password'], password):
+                    # The password is verified
+                    totp = pyotp.TOTP(totp_secret)
+                    token = totp.now()
+                    
+                    # Send the TOTP token to the user's email
+                    message = Mail(
+                        from_email='dev.pixera@gmail.com',
+                        to_emails=[email],
+                        subject='Two-Factor Authentication',
+                        plain_text_content=f'Your TOTP token is: {token}'
+                    )
+                    
+                    try:
+                        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+                        response = sg.send(message)
+                        flash('A TOTP token has been sent to your email. Please check your email and enter the token.')
+                    except Exception:
+                        flash('Failed to send the TOTP token.')
+                    
+                    # Redirect to the 2FA verification page
+                    return redirect(url_for('verify_2fa', email=email))
+            else:
+                flash('Login failed. Please check your email and password.')
+                return render_template('login.html')
         else:
-            flash('Login failed. Please check your email and password.')
+            flash('User not found. Please check your email and try again.')
     
     return render_template('login.html')
 
@@ -177,10 +185,21 @@ def reset_password(token):
         email = reset_tokens[token]
         if request.method == 'POST':
             new_password = request.form['new_password']
-            users_collection.update_one({'email': email}, {'$set': {'password': new_password}})
+            
+            if not new_password:
+                flash('Password is required.')
+                return redirect(url_for('reset_password', token=token))
+            
+            # Hash the new password
+            hashed_password = bcrypt.generate_password_hash(new_password).decode(os.getenv("DECODE_ALGORITHM"))
+            
+            # Update the user's password in the database
+            users_collection.update_one({'email': email}, {'$set': {'password': hashed_password}})
+            
             flash('Password reset successfully. You can now log in with your new password.')
             del reset_tokens[token]
             return redirect(url_for('login'))
+        
         return render_template('reset_password.html')
     else:
         flash('Invalid or expired token.')
